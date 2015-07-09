d6f88bc66c76f7963b4d989dcd213bbace7fdfe3
Dimitri Zacher <zacher.d@hotmail.de>
2015-07-09 21:48:22 +0200

    default_timezone

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,4 +39,10 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function init()
+    {
+        date_default_timezone_set( 'Europe/Berlin' );
+        parent::init();
+    }
 }
